7903674c8795c1ca82050a8f9b5105e7795a8da8
mpk <niboshiporipori@gmail.com>
2014-12-06 13:53:02 +0900

    変換結果をテキストエリア内に出力するようにviewを変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+	DEFAULT_FORM_MESSAGE = "ここに変換したい文章を入力してください."
   def home
+  	@initial_contents = (params[:org_text] != nil)? params[:org_text] : DEFAULT_FORM_MESSAGE
   	@short_text = ""
   	if params[:org_text] != nil
 	  	tw = TwLongText.new(params[:org_text])
