b44a6b61b3892cfa8da43b0a2b8eff0abbb0c6f0
mpk <niboshiporipori@gmail.com>
2014-12-07 13:53:34 +0900

    変換後のテキストを表示できるようにviewを変更 バージョンも表示 変換後のテキストの生成処理自体はまだできてない

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
 	DEFAULT_FORM_MESSAGE = "ここに変換したい文章を入力してください."
   def home
-  	@initial_contents = (params[:org_text] != nil)? params[:org_text] : DEFAULT_FORM_MESSAGE
+		@version = "v0.1.0"
+  	@org_text = (params[:org_text] != nil)? params[:org_text] : DEFAULT_FORM_MESSAGE
   	@short_text = ""
+  	@convert_text = ""
   	if params[:org_text] != nil
-	  	tw = TwLongText.new(params[:org_text])
+	  	tw = TwLongText.new(params[:org_text].dup)
 	  	@short_text = tw.to_short_text
 	  end
   end
