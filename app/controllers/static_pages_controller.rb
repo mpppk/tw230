a35fd0507a654aff21a4663a81dc93a75d7a6362
mpk <niboshiporipori@gmail.com>
2014-12-07 19:01:17 +0900

    変換後の文章の長さが140文字を超える場合は警告メッセージを出すように変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,7 @@ class StaticPagesController < ApplicationController
   	@short_text_length   = 0
   	@convert_text_length = 0
   	@comp_rate           = 0
+  	@short_text_error_message = ""
 
   	if params[:org_text] != nil
 	  	result = TwLongText.new(params[:org_text].dup).to_short_text
@@ -18,6 +19,10 @@ class StaticPagesController < ApplicationController
 	  	@short_text_length   = result[:short_text_length]
 	  	@convert_text_length = result[:convert_text_length]
 	  	@comp_rate           = (1.0 - @short_text_length.to_f/@org_text_length.to_f) * 100
+	  
+	  	if @short_text_length.to_i > 140
+	  		@short_text_error_message = "Warning! - 変換後の文字数が140文字を超えています！"
+	  	end
 	  end
   end
 
