670f42fc33c096f3c76aa4e9392b5558a2f08e2e
mpk <niboshiporipori@gmail.com>
2014-12-07 16:34:42 +0900

    初期値を指定していなかった問題を修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,15 @@
 class StaticPagesController < ApplicationController
-	DEFAULT_FORM_MESSAGE = "ここに変換したい文章を入力してください."
+	DEFAULT_FORM_MESSAGE = "ここに変換したい文章を入力してくださいお願いしますどうかこの通りです"
   def home
 		@version      = "v0.1.0"
   	@org_text     = (params[:org_text] != nil)? params[:org_text] : DEFAULT_FORM_MESSAGE
   	@short_text   = ""
   	@convert_text = ""
+  	@org_text_length     = 0
+  	@short_text_length   = 0
+  	@convert_text_length = 0
+  	@comp_rate           = 0
+
   	if params[:org_text] != nil
 	  	result = TwLongText.new(params[:org_text].dup).to_short_text
 	  	@short_text          = result[:short_text]
